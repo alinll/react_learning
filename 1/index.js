b9b031f5bfb7921647818fcdd9810cea1e9c34df
alinll <alineedemyanchuk@gmail.com>
2024-10-25 14:57:10 +0300

    react components

--- 1/index.js
+++
@@ -1,9 +0,0 @@
-//ReactDOM.render(<p>Hello World from React</p>, document.getElementById("root"));
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <div>
-    <h1>React Course</h1>
-    <p>We will learn react in this course.</p>
-  </div>
-);
