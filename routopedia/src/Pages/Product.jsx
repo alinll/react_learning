1e3301f642ba1ed71bffed5b6202dd884ef4b1c7
alinll <alineedemyanchuk@gmail.com>
2024-10-29 18:29:53 +0200

    RouteOPedia - Routing in React

---
+++ routopedia/src/Pages/Product.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+
+function Product() {
+  const navigate = useNavigate();
+  const [goToProduct, setGoToProduct] = useState(() => {
+    return false;
+  });
+
+  return (
+    <div>
+      Product
+      <button
+        onClick={() => {
+          navigate("/product/create");
+        }}
+      >
+        Add Product
+      </button>
+      <Link to={`/product/details/5`}>
+        <button>Navigate to Product Detail - 5</button>
+      </Link>
+      {goToProduct && <Navigate to="/product/details/3" />}
+      <button
+        onClick={() => {
+          setGoToProduct({ goToProduct: true });
+        }}
+      >
+        Navigate to Product - 3 (UseState)
+      </button>
+    </div>
+  );
+}
+
+export default Product;
---
+++ routopedia/src/Pages/ProductList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function ProductList() {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      ProductList
+      <button
+        onClick={() => {
+          navigate("/product/create");
+        }}
+      >
+        Add Product
+      </button>
+    </div>
+  );
+}
+
+export default ProductList;
