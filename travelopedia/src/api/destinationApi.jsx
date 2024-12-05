bd310ece7f43c377440944bce7d47edca31351cb
alinll <alineedemyanchuk@gmail.com>
2024-12-05 10:45:39 +0200

    TravelOPedia- Redux Toolkit, Query, Mutations and Slice

---
+++ travelopedia/src/api/destinationApi.jsx
@@ -0,0 +1,49 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const destinationApi = createApi({
+  reducerPath: "apidestination",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5001/" }),
+  tagTypes: ["Destinations"],
+  endpoints: (builder) => ({
+    getAllDestination: builder.query({
+      query: () => ({
+        url: "destination",
+        method: "GET",
+        params: {},
+      }),
+      transformResponse: (res) => res.sort((a, b) => b.id - a.id),
+      providesTags: ["Destinations"],
+    }),
+    addDestination: builder.mutation({
+      query: (destination) => ({
+        url: "destination",
+        method: "POST",
+        body: destination,
+      }),
+      invalidatesTags: ["Destinations"],
+    }),
+    updateDestination: builder.mutation({
+      query: (destination) => ({
+        url: `destination/${destination.id}`,
+        method: "PUT",
+        body: destination,
+      }),
+      invalidatesTags: ["Destinations"],
+    }),
+    deleteDestination: builder.mutation({
+      query: ({ id }) => ({
+        url: `destination/${id}`,
+        method: "DELETE",
+        body: id,
+      }),
+      invalidatesTags: ["Destinations"],
+    }),
+  }),
+});
+
+export const {
+  useGetAllDestinationQuery,
+  useAddDestinationMutation,
+  useUpdateDestinationMutation,
+  useDeleteDestinationMutation,
+} = destinationApi;
---
+++ travelopedia/src/components/AddDestination.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { useAddDestinationMutation } from "../api/destinationApi";
+
+function AddDestination() {
+  const [newCity, setNewCity] = useState("");
+  const [newCountry, setNewCountry] = useState("");
+  const [addDestinationMutation] = useAddDestinationMutation();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addDestinationMutation({
+      id: (Math.random() * 100).toString(),
+      city: newCity,
+      country: newCountry,
+      daysNeeded: parseInt(Math.random() * 10) + 1,
+    });
+    setNewCity("");
+    setNewCountry("");
+  };
+
+  return (
+    <div className="p-4 border">
+      <form onSubmit={handleSubmit}>
+        <div className="row col-8 offset-2">
+          <h4>Enter a new Destination</h4>
+          <div className="col-5 p-1">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter city..."
+              value={newCity}
+              onChange={(e) => setNewCity(e.target.value)}
+            ></input>
+          </div>
+          <div className="col-5 p-1">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter country..."
+              value={newCountry}
+              onChange={(e) => setNewCountry(e.target.value)}
+            ></input>
+          </div>
+          <div className="col-2 p-1">
+            <button className="btn btn-success form-control">Add</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default AddDestination;
